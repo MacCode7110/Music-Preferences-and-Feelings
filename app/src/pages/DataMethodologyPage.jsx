4246f90bf50474c6c4a4e655c583a6ec5db85f19
Matthew <memcalarney@wpi.edu>
2026-07-14 20:21:58 -0400

    Setup structure of DataMethodologyPage

--- app/src/pages/DataMethodologyPage.jsx
+++ app/src/pages/DataMethodologyPage.jsx
@@ -1,5 +1,25 @@
+import Heading from '../components/Heading'
+import Content from '../components/Content'
+
 const DataMethodologyPage = () => {
-  return <div>DataMethodologyPage</div>
+  return (
+    <section className="section">
+      <div className="container">
+        <header className="block mb-3">
+          <Heading size={1} className="is-family-primary has-text-black">
+            Data Methodology
+          </Heading>
+          <Content size={5} className="is-family-secondary has-text-black p-5"></Content>
+        </header>
+        <main className="block mb-3">
+          <Content size={5} className="is-family-secondary has-text-black p-5"></Content>
+        </main>
+        <footer className="block mb-3">
+          <Content size={5} className="is-family-secondary has-text-black p-5"></Content>
+        </footer>
+      </div>
+    </section>
+  )
 }
 
 export default DataMethodologyPage
